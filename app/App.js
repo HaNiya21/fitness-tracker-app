4d465251ae47fff556d9747c9b00be75ba3901ca
Lina Urrego <urrego.linam@gmail.com>
2024-10-24 23:20:20 -0400

    standby app file

---
+++ app/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import LandingPage from './LandingPage';
+import Login from './Login';
+
+const App = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>    
+    );
+};
+
+export default App;
