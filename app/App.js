29511f7dafebfd3927aa48b13275eaa517dd773a
Lina Urrego <urrego.linam@gmail.com>
2024-10-31 16:20:52 -0400

    changes to app.py

--- app/App.js
+++ app/App.js
@@ -1,24 +1,23 @@
-// App.js
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import Login from './Login';
 import SignUp from './SignUp';
-import 'react-native-gesture-handler';
+import WaterIntake from './WaterIntake';
+import footer from './footer';
 
-const Stack = createNativeStackNavigator();
-
-const App = () => {
+function App() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="LandingPage" component={LandingPage} />
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="SignUp" component={SignUp} />
-            </Stack.Navigator>
-        </NavigationContainer>  
+        <Router>
+            <Switch>
+                <Route path="/" exact component={LandingPage} />
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={SignUp} />
+                <Route path="/waterintake" component={WaterIntake} />
+                <Route path="/footer" component={footer} />
+            </Switch>
+        </Router>
     );
-};
+}
 
 export default App;
