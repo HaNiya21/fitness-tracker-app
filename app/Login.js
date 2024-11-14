d6e3ff2e3f655848f71628d27459e42f13fc2b11
Hana Geremew <hanaberehanu@gmail.com>
2024-11-14 00:12:30 -0500

    addded android files

--- app/Login.js
+++ app/Login.js
@@ -57,7 +57,7 @@ const Login = () => {
             const normalizedEmail = formValues.email.toLowerCase(); // Convert to lowercase
     
             try {
-                const response = await fetch('http://localhost:5000/api/login', {
+                const response = await fetch('http://172.20.9.103:5000/api/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
